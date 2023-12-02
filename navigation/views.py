5fc8b656c11b44840a15cf2440e98d85544645d2
dmmonir2003 <dmmonir2003@gmail.com>
2023-12-02 23:11:02 +0600

    filter add

--- navigation/views.py
+++ navigation/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(request):
-    return render(request,('about.html'))
+    days=[
+{'name': 'Dave', 'age': 22},
+{'name': 'Joe', 'age': 31},
+{'name': 'Josh', 'age': 19},
+]
+    return render(request,('about.html'),{'daya':days})
 
 def contact(request):
     return render(request,('contact.html'))
